12e53da77043413a96347c32362d7467ed26121c
EdwardGoomba <ask@edwardvdesign.com>
2018-01-04 16:46:44 -0500

    Added tags and pdf download warning

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
+
+$secureLinks.attr('target', '_blank');
+$pdfs.attr('download', true);
+
+$secureLinks.addClass('secure');
+$pdfs.addClass('pdf');
+
+$pdfs.on('click', function(event) {
+
+  if ($(':checked').length === 0) {
+    event.preventDefault();
+    alert('Please check the box to allow pdf downloads');
+  }
+});
