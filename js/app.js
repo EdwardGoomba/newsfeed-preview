b1c9572f6cb292fcf8573642becd5c37a865e8df
EdwardGoomba <ask@edwardvdesign.com>
2018-01-04 16:57:46 -0500

    Make js unobtrusive

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
+const $pdfCheckbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
 
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
@@ -15,3 +16,5 @@ $pdfs.on('click', function(event) {
     alert('Please check the box to allow pdf downloads');
   }
 });
+
+$('#links').append($pdfCheckbox);
